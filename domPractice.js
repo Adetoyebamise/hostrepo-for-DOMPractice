e495a735606dccbfd87e91c8641f401efa8e2474
Adetoye Babalola <adetoyebamise@gmail.com>
2021-03-20 22:03:19 +0100

    indent properly

--- domPractice.js
+++ domPractice.js
@@ -59,3 +59,193 @@
   submitButton.addEventListener('click', sendItemFourFeedback);
 
   submitButton.addEventListener('click', showOutputFeedbackItemSelection);
+
+
+
+               /*  DOCUMENT OBJECT MODEL ASSIGNMENT */
+
+
+               /* Examine the Document Model        */
+ 
+//  console.dir(document);    //shows us all of the different property and method in the document
+//  console.log(document);
+//  console.log(document.domain);
+//  console.log(document.URL);
+//  console.log(document.title);
+//  document.title = 'Understanding JavaScript with Don';  //changes the title of the document
+//  console.log(document.doctype);
+//  console.log(document.head);
+//  console.log(document.body);
+//  console.log(document.all);    //can be used to select or replace object but not the best way
+//  console.log(document.all[13]);
+//  document.all[13].textcontent = 'string'
+//  console.log(document.forms)
+//  console.log(document.links)
+//  console.log(document.images)
+
+               /* METHOD TO QUERY THE DOM    a. getelementbyid */
+
+//  console.log(document.getElementById('header-title'))
+//  const headerTitle = document.getElementById('header-title')
+//  console.log(headerTitle)                       
+//  headerTitle.textContent = "JavaScript"
+//  headerTitle.innerText = ""
+//  console.log(headerTitle.innerText)
+//  headerTitle.innerHTML = '<h3></h3>'
+//  header.style.borderBottom = "solid 3px #000"
+
+              /* METHOD TO QUERY THE DOM  b. getElementbyClassName */
+
+//  const items = document.getElementsByClassName('listGroupItem');
+//  console.log(items)
+//  console.log(items[1]) 
+//  items[1].textContent = ""
+//  items[1].style.fontWeight = ""
+//  items[1].style.backgroundColor = ""
+
+//  items.style.backgroundColor = "#f4f4f4"
+
+//  for (var i; i < items.length; i++) {
+//      items[i].style.backgroundColor = '#f4f4f4'
+//        }
+
+               /*  METHOD TO QUERY THE DOM c. getElementByTagName */
+
+//  const li = document.getElementsByTagName('list-group-item')
+//  console.log(li)
+//  console.log(li[1]) 
+//  li[1].textContent = "Headset"
+//  li[1].style.fontWeight = ""
+//  li[1].style.backgroundColor = ""
+//  items.style.backgroundColor = "#f4f4f4"
+
+//  for (var i; i < li.length; i++) {
+//     li[i].style.backgroundColor = '#f4f4f4'
+//        }
+
+               /*  METHOD TO QUERY THE DOM d. QuerySelector  */
+
+//  const header = document.querySelector('#updateItem')
+//  header.style.borderBottom = " solid 4px #ccc"
+
+//  const input = document.querySelector( '.input')
+//  input.value = "Hello World"
+
+//  const submit = document.querySelector('input[type="submit"]')
+//  submit.value="SEND"
+
+//  const item = document.querySelector('.list-group-item')
+//  item.style.color = 'red'
+
+//  const lastitem = document.querySelector('.list-group-item : last-child')
+//  lastitem.style.color = 'blue'
+
+//  const secondItem = document.querySelector('.list-group-item : nth-child(2)')
+//  secondItem.style.color = 'coral'
+
+               /*  METHOD TO QUERY THE DOM e. QuerySelectorAll */
+
+//  const titles = document.querySelectorAll('.title')
+
+//  console.log(titles)
+//  titles[0].textContent = 'Hello'
+
+//  const odd = document.querSelectorAll('li:nth-child(odd)')
+//  const even = document.querSelectorAll('li:nth-child(even)')
+
+//  for (var i = 0; i < odd.length; i++) {
+//      odd[i].style.backgroundColor = '#f4f4f4'
+//      even[i].style.backgroundColor = '#ccc'
+//        }
+
+               /*   Tranversing the DOM     */
+
+//  const itemList = document.querySelector('#items');
+
+               /*   parentNode  */
+
+//  console.log(itemList.parentNode);
+//  itemList.parentNode.style.backgroundColor = '#f4f4f4'
+//  console.log(itemList.parentNode.parentNode.parentNode)
+
+
+               /*    parentElement */
+
+//  console.log(itemList.parentElement);
+//  itemList.parentElement.style.backgroundColor = '#f4f4f4'
+//  console.log(itemList.parentElement.parentElement.parentElement)
+
+
+               /*    childnode     */
+
+//  console.log(itemList.childNodes)
+//  console.log(itemList.children)
+//  console.log(itemList.children[1])
+//  itemList.children[1].style.backgroundColor = 'yellow'
+
+               /*    FirstChild    */
+
+//  console.log(itemList.firstChild)
+ 
+               /*    FirstElementChild   */
+
+//  console.log(itemList.firstElementChild)
+//  itemList.firstElementChild.textContent = ''
+
+               /* lastChild    */
+
+//  console.log(itemList.lastChild)
+ 
+               /* lastElementChild    */
+
+//  console.log(itemList.lastElementChild)
+//  itemList.lastElementChild.textContent = ''
+
+               /*  nextSiblings  */
+
+//  console.log(itemList.nextSibling)
+
+               /* nextElementSibling    */
+
+//  console.log(itemList.nextElementSibling)
+
+               /* previousSibling     */
+
+//  console.log(itemList.previousSibling)
+
+               /* previousElementSibling    */
+
+//  console.log(itemList.previousElementSibling)
+//  itemList.previousElementSibling.style.color = ''
+
+               /*  You dont need Jquery for simple DOM manipulation   */
+
+               /*  createElement and  create div    */
+
+//  const newDiv = document.createElement('div')
+
+               /*  Add class    */
+
+//  newDiv.className = ''
+
+               /* Add id       */
+
+//  newDiv.id = ''
+
+               /* Add attribute   */
+
+//  newDiv.setAttribute('', '')
+
+               /* create a text node  */
+
+//  const newDivText = document.createTextNode('')
+
+               /* Add text to div   */
+
+//  newDiv.appendChild(newDivText)
+
+//  const container = document.querySelector('header.container')
+//  const h1 = document.querySelector('')
+//  console.log(newDiv)
+//  newDiv.style.fontSize = ''
+//  container.insertBefore( )
