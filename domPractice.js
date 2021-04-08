7e1fe061a0f35ca6850c6c7d702009787742619f
Adetoye Babalola <adetoyebamise@gmail.com>
2021-04-08 11:43:45 +0100

    feature-version-2 release: initial commit for the second version; functional todo list, with practice on how to tranverse the DOM.

--- domPractice.js
+++ domPractice.js
@@ -1,252 +1,229 @@
-// Declaring variable using the DOM 
-   const clickMeButton= document.querySelector('.click-me-button');
-   const outputDiv = document.querySelector('.output');
-   const unorderedItemListOne = document.querySelector('#unorderedItemList > ul:nth-child(1) >  li:nth-child(1)');
-   const nameInputField = document.querySelector('.item-input');
-
-//  Query selector for the Submitt button
-  const submitButton =document.querySelector('#submitButton');
-
-//  Functions to change the output text when the click here button is click.
-  const giveClickHereFeedback = () => {
-   outputDiv.innerText = 'Please Enter the form above ';
-   };
-
-  const replyItemOneFeedback = () => {
-   unorderedItemListOne.innerText = nameInputField.value ;
-   };
-
-  const showOutputFeedback = () => {
-     outputDiv.innerText = 'You can add another Item';
-   };
-
-// Declare another variable and function for item two
-   const unorderedItemListTwo = document.querySelector('#unorderedItemList > ul:nth-child(1) > li:nth-child(2)');
-
-  const tellItemTwoFeedback = () => {
-   unorderedItemListTwo.innerText = nameInputField.value ;
-    };
-// Declare variable and function for item three
-  const unorderedItemListThree = document.querySelector('#unorderedItemList > ul:nth-child(1) > li:nth-child(3)');
-
-  const reportItemThreeFeedback = () => {
-   unorderedItemListThree.innerText = nameInputField.value ;
-   };
-// Declare variable and function for item four
-  const unorderedItemListFour = document.querySelector('#unorderedItemList > ul:nth-child(1) >  li:nth-child(4)');
-
-  const sendItemFourFeedback = () => {
-   unorderedItemListFour.innerText = nameInputField.value ;
-    };
-
-  const showOutputFeedbackItemSelection = () => {
-      outputDiv.innerText = 'You can only add four items';
-    };
-
-// Event Listener Method for submit and click Here button.
-
-   clickMeButton.addEventListener('click', giveClickHereFeedback);
-
-   submitButton.addEventListener('click', replyItemOneFeedback);
-
-   submitButton.addEventListener('click', showOutputFeedback);
-
-   submitButton.addEventListener('click', tellItemTwoFeedback);
-
-   submitButton.addEventListener('click', showOutputFeedback);
- 
-   submitButton.addEventListener('click', reportItemThreeFeedback);
-
-   submitButton.addEventListener('click', sendItemFourFeedback);
-
-   submitButton.addEventListener('click', showOutputFeedbackItemSelection);
-
-
-
-               /* ===  DOCUMENT OBJECT MODEL ASSIGNMENT  ===  */
-
-
-               /* ====  Examine the Document Model     ====   */
- 
-  console.dir(document);    //shows us all of the different property and method in the document
-  console.log(document);
-  console.log(document.domain);
-  console.log(document.URL);
-  console.log(document.title);
-  document.title = 'Understanding JavaScript with Don';  //changes the title of the document
-  console.log(document.doctype);
-  console.log(document.head);
-  console.log(document.body);
-  console.log(document.all);    //can be used to select or replace object but not the best way
-  console.log(document.all[13]);
-  document.all[13].textcontent = 'string'
-  console.log(document.forms)
-  console.log(document.links)
-  console.log(document.images)
-
-               /* METHOD TO QUERY THE DOM    a. getelementbyid */
-
-  console.log(document.getElementById('header-title'))
-  const headerTitle = document.getElementById('header-title')
-  console.log(headerTitle)                       
-  headerTitle.textContent = "JavaScript"
-  headerTitle.innerText = ""
-  console.log(headerTitle.innerText)
-  headerTitle.innerHTML = '<h3></h3>'
-  header.style.borderBottom = "solid 3px #000"
-
-              /* METHOD TO QUERY THE DOM  b. getElementbyClassName */
-
-  const items = document.getElementsByClassName('listGroupItem');
-  console.log(items)
-  console.log(items[1]) 
-  items[1].textContent = ""
-  items[1].style.fontWeight = ""
-  items[1].style.backgroundColor = ""
-
-  items.style.backgroundColor = "#f4f4f4"
-
-  for ( index; index < items.length; index++) {
-     items[index].style.backgroundColor = '#f4f4f4'
-       }
-
-               /*  METHOD TO QUERY THE DOM c. getElementByTagName */
-
-  const li = document.getElementsByTagName('list-group-item')
-  console.log(li)
-  console.log(li[1]) 
-  li[1].textContent = "Headset"
-  li[1].style.fontWeight = ""
-  li[1].style.backgroundColor = ""
-  items.style.backgroundColor = "#f4f4f4"
-
-  for ( index; index < li.length; i++) {
-    li[i].style.backgroundColor = '#f4f4f4'
-       }
-
-               /*  METHOD TO QUERY THE DOM d. QuerySelector  */
-
-  const header = document.querySelector('#updateItem')
-  header.style.borderBottom = " solid 4px #ccc"
-
-  const input = document.querySelector( '.input')
-  input.value = "Hello World"
-
-  const submit = document.querySelector('input[type="submit"]')
-  submit.value="SEND"
-
-  const item = document.querySelector('.list-group-item')
-  item.style.color = 'red'
-
-  const lastitem = document.querySelector('.list-group-item : last-child')
-  lastitem.style.color = 'blue'
-
-  const secondItem = document.querySelector('.list-group-item : nth-child(2)')
-  secondItem.style.color = 'coral'
-
-               /*  METHOD TO QUERY THE DOM e. QuerySelectorAll */
-
-  const titles = document.querySelectorAll('.title')
-
-  console.log(titles)
-  titles[0].textContent = 'Hello'
-
-  const odd = document.querSelectorAll('li:nth-child(odd)')
-  const even = document.querSelectorAll('li:nth-child(even)')
-
-  for ( index = 0; index < odd.length; i++) {
-     odd[index].style.backgroundColor = '#f4f4f4'
-     even[index].style.backgroundColor = '#ccc'
-       }
-
-               /*   Tranversing the DOM     */
-
-  const itemList = document.querySelector('#items');
-
-               /*   parentNode              */
-
-  console.log(itemList.parentNode);
-  itemList.parentNode.style.backgroundColor = '#f4f4f4'
-  console.log(itemList.parentNode.parentNode.parentNode)
-
-
-               /*    parentElement          */
-
-  console.log(itemList.parentElement);
-  itemList.parentElement.style.backgroundColor = '#f4f4f4'
-  console.log(itemList.parentElement.parentElement.parentElement)
-
-
-               /*    childnode              */
-
-  console.log(itemList.childNodes)
-  console.log(itemList.children)
-  console.log(itemList.children[1])
-  itemList.children[1].style.backgroundColor = 'yellow'
-
-               /*    FirstChild             */
-
-  console.log(itemList.firstChild)
- 
-               /*    FirstElementChild      */
-
-  console.log(itemList.firstElementChild)
-  itemList.firstElementChild.textContent = ''
-
-               /* lastChild                 */
-
-  console.log(itemList.lastChild)
- 
-               /* lastElementChild          */
-
-  console.log(itemList.lastElementChild)
-  itemList.lastElementChild.textContent = ''
-
-               /*  nextSiblings             */
-
-  console.log(itemList.nextSibling)
-
-               /* nextElementSibling        */
-
-  console.log(itemList.nextElementSibling)
-
-               /* previousSibling           */
-
-  console.log(itemList.previousSibling)
-
-               /* previousElementSibling    */
-
-  console.log(itemList.previousElementSibling)
-  itemList.previousElementSibling.style.color = ''
-
-               /*  You dont need Jquery for simple DOM manipulation   */
-
-               /*  createElement and  create div    */
-
-  const newDiv = document.createElement('div')
-
-               /*  Add class    */
-
-  newDiv.className = ''
-
-               /* Add id       */
-
-  newDiv.id = ''
-
-               /* Add attribute   */
-
-  newDiv.setAttribute('', '')
-
-               /* create a text node  */
-
-  const newDivText = document.createTextNode('')
-
-               /* Add text to div   */
-
-  newDiv.appendChild(newDivText)
-
-  const container = document.querySelector('header.container')
-  const h1 = document.querySelector('')
-  console.log(newDiv)
-  newDiv.style.fontSize = ''
-  container.insertBefore( )
+// declaring variable: querySelector Method
+  const itemInput = document.querySelector("#itemInput")
+  const submitButton = document.querySelector("#submitButton");
+  const clearItems = document.querySelector(".clear-items");
+  const form = document.querySelector(".form-container");
+  const independentContainer = document.querySelector(".independent-container");
+  const majorContainer = document.querySelector(".major-content-container");
+  const outputDisplay = document.querySelector(".output-alert");
+  const targetItem = document.querySelector(".unordered-item-list > ul:nth-child(1) > li:nth-child(1)");
+  
+// edit option
+  let editElement;
+  let editingFlag = false;
+  let editID = "";
+
+// event listeners
+  submitButton.addEventListener("click", addItem)
+// clear items event listeners
+  clearItems.addEventListener("click", clearItem);
+// load Items
+window.addEventListener("DOMContentLoaded", setupItem);
+// functions
+  function addItem(eventObject) {
+    eventObject.preventDefault();
+    const value = itemInput.value
+
+// generate unique number for local storage using Date object
+  const uniqueId = new Date().getTime();
+  if (value  && !editingFlag) {
+    createListItem(uniqueId, value)
+  //   // display output
+    displayOutput("Item added to the list", "success");
+    // show container
+    majorContainer.classList.add("show-container");
+    // add to local storage
+    addToLocalStorage(uniqueId, value);
+    // set back to default
+    setBackToDefault();
+  } else if (value  && editingFlag) {
+    editElement.textContent = value;
+    displayOutput("value changed", "success");
+    // edit local storage
+    editLocalStorage(editID, value);
+    setBackToDefault();
+  } else {
+    displayOutput("Please Enter Value", "danger");
+  }
+};
+
+// display Output
+  function displayOutput(text, action) {
+    outputDisplay.textContent = text;
+    outputDisplay.classList.add(`output-alert-${action}`);
+  // remove alert within a specific period 
+  setTimeout(function removeOutputDisplay() {
+    outputDisplay.textContent = "";
+    outputDisplay.classList.remove(`output-alert-${action}`)
+  }, 1000);
+  };
+
+//  function clear Items list
+ function clearItem() {
+  const itemList = document.querySelectorAll(".unordered-item-list");
+  if(itemList.length > 0){
+   itemList.forEach(function(item){
+     independentContainer.removeChild(item);
+   })
+  };
+  majorContainer.classList.remove("show-container");
+    displayOutput("empty list", "danger");
+    setBackToDefault();
+     localStorage.removeItem("targetItem");
+  };
+
+// delete Item function
+  function deleteItem(eventObject) {
+    const element = eventObject.currentTarget.parentElement.parentElement;
+    const uniqueId = element.dataset.uniqueId;
+    independentContainer.remove(element);
+    if(majorContainer.children.length === 0){
+      majorContainer.classList.remove("show-container");
+    }
+    displayOutput("item removed", "danger");
+    setBackToDefault();
+    // remove from local storage
+    removeFromLocalStorage(uniqueId);
+  };
+
+// edit items function
+  function editItem(eventObject) {
+  const element = eventObject.currentTarget.parentElement.parentElement;
+  // set edit item
+  editElement = eventObject.currentTarget.parentElement.parentElement;
+  // set form value
+  itemInput.value = editElement.textContent;
+  editingFlag = true;
+
+  editID = element.dataset.id;
+  submitButton.value = "edit";
+  };
+
+// set back to default
+  function setBackToDefault() {
+    itemInput.value = ""; 
+    editingFlag = false;
+    editID = "";
+    submitButton.value = "submit";
+    console.log("set back to default"); 
+  };
+
+// local storage
+  function addToLocalStorage(uniqueId, value){
+    const listGroup = {uniqueId:uniqueId, value:value};
+    // let items = localStorage.getItems("targetItem");
+     let items = localStorage.getItem("targetItem")? JSON.parse(localStorage.getItem("targetItem")) : [];
+     console.log(items);
+     items.push(listGroup);
+    localStorage.setItem("targetItem", JSON.stringify(items));
+    
+    //console.log("added to local storage")
+  };
+  function removeFromLocalStorage(uniqueId) {
+    let items = getLocalStorage();
+
+    items = items.filter(function(item){
+      if(item.uniqueId !== uniqueId){
+        return item;
+      }
+    })
+    localStorage.setItem("targetItem", JSON.stringify(items));
+  };
+  function editLocalStorage(uniqueId, value){
+    let items = getLocalStorage();
+    items = items.map(function(item){
+      if(item.uniqueId === uniqueId){
+        itemInput.value = value ;
+      }
+      return item;
+    })
+    localStorage.setItem("targetItem", JSON.stringify(items));
+  };
+  function getLocalStorage(){
+    return localStorage.getItem("targetItem")? JSON.parse(localStorage.getItem("targetItem")) : [];
+  };
+// localStorage API
+// setItem
+// getItem
+// removeItem
+//save as strings
+  // localStorage.setItem("orange", JSON.stringify(["item1", "item2"]));
+  // const oranges =  JSON.parse(localStorage.getItem("orange"));
+  // console.log(oranges);
+  // localStorage.removeItem("orange");
+// ===== setup items   ====
+  function setupItem() {
+    let items = getLocalStorage();
+    if(items.length > 0){
+      items.forEach(function(item) {
+        createListItem(item.uniqueId, item.value)
+      })
+      majorContainer.classList.add("show.container");
+    }
+  };
+
+  function createListItem(uniqueId, value) {
+    // create element
+   const element = document.createElement("div");
+   // add class
+   element.classList.add("unordered-item-list");
+   // add id
+   let attr  = document.createAttribute("data-id");
+   attr.value = uniqueId;
+   element.setAttributeNode(attr);
+   element.innerHTML = `
+    <ul>
+      <li>${value} 
+        <div class="button-container"> 
+        <input type ="button" class= "edit-button" value = "Edit Item"> 
+        <input type ="button" class= "delete-button" value = "Delete Item"> 
+        </div>
+      </li>
+    </ul>
+    `;
+    // this is where we can set those functions
+     const deleteButton = element.querySelector(".delete-button");
+     deleteButton.addEventListener("click", deleteItem);
+     const editButton = element.querySelector(".edit-button");
+     editButton.addEventListener("click", editItem);
+    // append child 
+    independentContainer.appendChild(element);
+  }
+
+/* ==========     Exercise  ========== */
+// select class characters with document.querySelector
+// const findCharacters = document.querySelectorAll(".characters");
+// console.log(findCharacters);
+// // select class humans from class characters
+// const findHuman = document.querySelector(".humans");
+// console.log(findHuman);
+// // select all humans with querySelectorAll, starting from humans
+// const findHumanAgain = document.querySelectorAll(".humans");
+// const allHumans = findHumanAgain[0].children;
+// console.log(findHumanAgain[0]);
+// console.log(allHumans);
+// // select all hobbit with children
+// const findHobbits = document.querySelector(".hobbits")
+// console.log(findHobbits);
+// console.log((document.querySelectorAll(".hobbits")[0].children));
+// // Select the Merry (the hobbit)
+// console.log(findHobbits.children[2]);
+// // Select .enemies from Sauron
+// const findEnemies = document.querySelector(".enemies");
+// console.log(findEnemies.children[0].parentElement);
+// // Select the .characters div from Nazgûl
+// console.log(findEnemies.children[1].parentElement.parentElement);
+// // Select Elrond from Glorfindel
+// const findElves = document.querySelector(".elves");
+// console.log(findElves);
+// const selectingElrond = findElves.children;
+// console.log(selectingElrond); //returns HTML collection
+// const cherrypickingElrond = findElves.children[2];
+// console.log(cherrypickingElrond); //return the element
+// console.log(cherrypickingElrond.previousElementSibling);
+// // Select Legolas from Glorfindel
+// const selectingGlorfindel = cherrypickingElrond ;
+// console.log(selectingGlorfindel.previousElementSibling.previousElementSibling);
+
+// // Select Arwen from Glorfindel
+// console.log(findElves.children[1].nextElementSibling.nextElementSibling);
